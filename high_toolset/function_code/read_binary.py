1f331594e99b42dc7689cd7f4149d0724c54a9ce
苏雨 <45283702+suyu13020027161@users.noreply.github.com>
2024-08-01 15:21:03 +0100

    Add files via upload

--- high_toolset/function_code/read_binary.py
+++ high_toolset/function_code/read_binary.py
@@ -2,15 +2,36 @@
 from plyfile import PlyData
 
 # 读取PLY文件
-ply = PlyData.read('/home/ysu/Miscanthus/avg_high/miscanthus_23_08_09_UTM_binary.ply')
+filepath = '/home/ysu/Miscanthus/toolset/data/coord_transfer/Walled Garden 24 07 29_utm.ply'
+ply = PlyData.read(filepath)
 
 # 获取顶点信息
 vertex = ply['vertex']
 
+
+vertex_count = 0
+with open(filepath, 'rb') as file:
+    while True:
+        line = file.readline()
+        if b"end_header" in line:
+            break
+        if b"element vertex" in line:
+            parts = line.split()
+            pointnum = int(parts[2])  
+
+
+
+
+
+
+i = 0
 # 打印每个点的XYZ坐标
-for data in vertex:
-    x, y, z = data['x'], data['y'], data['z']
-    print(f"X: {x}, Y: {y}, Z: {z}")
+for data in vertex:   
+    x, y, z, r, g, b = data['x'], data['y'], data['z'], data['red'], data['green'], data['blue']
+    print(f"X: {x}, Y: {y}, Z: {z}, R: {r}, G: {g}, B: {b}")
     x = float(x)
-    print(x)
+    progress = (i / pointnum)*100
+    pp = str(progress)
+    print("\rProgress: " + pp + "%", end="         ")
+    i = i + 1
 
